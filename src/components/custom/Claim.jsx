93adda9ad4b9ff693472aacec42b7f2d7e30ccaf
Syed Hasnain Mehadi <hasnain.mehadi@neosoftmail.com>
2024-11-28 19:04:16 +0530

    finalize level 1 code

--- src/components/custom/Claim.jsx
+++ src/components/custom/Claim.jsx
@@ -9,10 +9,29 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import { useSelector } from "react-redux";
+import { getClaim } from "@/store/claims/claimApi";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const ClaimList = () => {
   const { claim } = useSelector((store) => store);
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handleNext = async () => {
+    if (Math.ceil(claim?.claims?.count / 10) > currentPage) {
+      await dispatch(getClaim(currentPage + 1));
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePrevious = async () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 1));
+      await setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
   return (
     <div className="grow bg-white p-4 rounded">
       <Card className="shadow-md">
@@ -24,6 +43,7 @@ const ClaimList = () => {
             <table className="w-full border-collapse text-sm text-left text-gray-600">
               <thead className="bg-gray-100 border-b">
                 <tr>
+                  <th className="px-4 py-2 text-gray-800"># ID</th>
                   <th className="px-4 py-2 text-gray-800">Amount</th>
                   <th className="px-4 py-2 text-gray-800">Remark</th>
                   <th className="px-4 py-2 text-gray-800">Expense Date</th>
@@ -41,20 +61,18 @@ const ClaimList = () => {
                       key={el.id}
                       className="border-b hover:bg-gray-50 transition"
                     >
+                      <td className="px-4 py-2">{el?.id}</td>
                       <td className="px-4 py-2">{el.claim_amount}</td>
                       <td className="px-4 py-2">{el.remark}</td>
+                      <td className="px-4 py-2">{el.expense_date}</td>
                       <td className="px-4 py-2">
                         {el.createdAt?.substring(0, 10)}
                       </td>
-                      <td className="px-4 py-2">{el.expense_date}</td>
                       <td className="px-4 py-2">{el.location?.name}</td>
                       <td className="px-4 py-2 text-center space-x-2">
                         <Button variant="secondary" size="sm">
                           View Bill
                         </Button>
-                        {/* <Button variant="destructive" size="sm">
-                          Delete
-                        </Button> */}
                       </td>
                     </tr>
                   ))}
@@ -62,21 +80,21 @@ const ClaimList = () => {
             </table>
           </div>
         </CardContent>
-        {claim?.claims?.count > 5 && (
+        {claim?.claims?.count > 10 && (
           <div className="m-5">
             <Pagination>
               <PaginationContent>
                 <PaginationItem>
-                  <PaginationPrevious href="#" />
+                  <PaginationPrevious onClick={handlePrevious} />
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationLink href="#">1</PaginationLink>
+                  <PaginationLink>{currentPage}</PaginationLink>
                 </PaginationItem>
                 <PaginationItem>
                   <PaginationEllipsis />
                 </PaginationItem>
                 <PaginationItem>
-                  <PaginationNext href="#" />
+                  <PaginationNext onClick={handleNext} />
                 </PaginationItem>
               </PaginationContent>
             </Pagination>
